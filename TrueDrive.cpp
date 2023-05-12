4d7595eb5653ab4ac647d9ed1a74621997acddb3
ojuniormunguia <oscarmunguiasotelo@gmail.com>
2023-05-11 23:15:31 -0600

    Versión funcional

--- TrueDrive.cpp
+++ TrueDrive.cpp
@@ -1,96 +1,120 @@
-#include<iostream>
-#include<vector>
-#include<iomanip>
-#include<string>
-#include<conio.h>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
 #include "InfoClass.h"
 
 using namespace std;
 
-bool checkForItemInt(vector<int> arr, int data);
-bool checkForItemInt(vector<string> arr, string data);
-void printTable(vector<int> placa, vector<string> motor, vector<string> modelo, vector<int>año, vector<string>nombre, vector<string>apellidos, vector<int>dui, vector<int>seguro, vector<int>telefono);
+bool checkForItemInt(const vector<int>& arr, int data);
+bool checkForItemString(const vector<string>& arr, const string& data);
+void printTable(const vector<int>& placa, const vector<string>& motor, const vector<string>& modelo);
 
-int main(){
-	char respuesta='\0';
-	int opciones;
-	int placataxi;
-	char confirmacion;
-	int placataxiToReinsert;
-	string driverName;
-	string plateNumber;
-	cout<<"Bienvenido a TrueDrive\t\n";
-	vector<int> placa;
-	vector<string> motor;
-	vector<string> modelo;
-	vector<int> año;
-	vector<string> nombre;
-	vector<string> apellidos;
-	vector<int> dui;
-	vector<int> seguro;
-	vector<int> telefono;
-	vector<int> nAplaca;
-	vector<string> nAmotor;
-	vector<string> nAmodelo;
-	vector<int> nAaño;
-	vector<string> nAnombre;
-	vector<string> nAapellidos;
-	vector<int> nAdui;
-	vector<int> nAseguro;
-	vector<int> nAtelefono;
-	do{
-		system("cls");
-		cout<<"Main menu opcioness:\n\n"<<endl;
-		cout<<"\t\t1. Add New Taxi to standby queue"<<endl;
-		cout<<"\t\t2. Check for actual data"<<endl;
-		cout<<"\t\t3. Sent Taxi to a Client"<<endl;
-		cout<<"\t\t4. Check for taxis in routes"<<endl;
-		cout<<"\t\t5. Reinsert a taxi to avaialable queue"<<endl;
-		cout<<"\t\t6. Exit program"<<endl<<endl;
-		cout<<"Please, input your opciones: ";
-		cin>>opciones;
-		switch(opciones){
-			case 1:
-				do{
-					cout<<"Taxi placataxi: ";
-					cin>>placataxi;
-				}while(checkForItemInt(taxis, placataxi));
-				taxis.push_back(placataxi);
-				do{
-					cout<<"Driver Name: ";
-					cin.ignore();
-					getline(cin,driverName);
-				}while(checkForItemInt(drivers, driverName));
-				drivers.push_back(driverName);
-				do{
-					cout<<"Plate Number: ";
-					cin.ignore();
-					getline(cin,plateNumber);
-				}while(checkForItemInt(plates, plateNumber));
-				plates.push_back(plateNumber);
-			break;
-			case 2:		
-				printTable(taxis, drivers, plates);
-			break;
-			case 3:
-				//code logic for sending taxis here
-			break;
-			case 4:
-				//code logic for on route checks
-			break;
-			case 5:
-				//code logic for reinset taxis into avaialable queue
-			break;
-			case 6:
-				cout<<"Program has ended...";
-				system("pause");
-				system("exit");
-			break;
-			default:
-				cout<<"Non correct opciones has been chosen."<<endl;
-			break;
-		}
-  	cout<<"Desea continuar? (Y/N)?: ";
-  	respuesta=getch();
-	}while(respuesta=='Y' || respuesta=='y');	
+int main() {
+    char respuesta = '\0';
+    int opciones;
+    int placataxi;
+    string motortaxi;
+    string modelotaxi;
+    vector<int> placa;
+    vector<string> motor;
+    vector<string> modelo;
+
+    cout << "Bienvenido a TrueDrive" << endl;
+
+    do {
+        system("cls");
+        cout << "Por favor seleccione una opción:\n\n";
+        cout << "\t\t1. Ingresar nuevo vehículo y conductor" << endl;
+        cout << "\t\t2. Comenzar Viaje" << endl;
+        cout << "\t\t3. Terminar Viaje" << endl;
+        cout << "\t\t4. Ver lista de conductores" << endl;
+        cout << "\t\t5. Salir del programa" << endl << endl;
+        cout << "Escribir su opción: ";
+        cin >> opciones;
+
+        switch (opciones) {
+            case 1:
+                cout << "Ingresando Vehículo..." << endl;
+                cout << "Ingrese placa del vehículo: ";
+                                cin >> placataxi;
+                placa.push_back(placataxi);
+
+                cout << "Ingrese número de motor: ";
+                cin >> motortaxi;
+                motor.push_back(motortaxi);
+
+                cout << "Ingrese modelo del vehículo: ";
+                cin.ignore();
+                getline(cin, modelotaxi);
+                modelo.push_back(modelotaxi);
+                break;
+
+            case 2:
+                printTable(placa, motor, modelo);
+                break;
+
+            case 3:
+                // Code logic for ending a trip
+                break;
+
+            case 4:
+                // Code logic for viewing the list of drivers
+                break;
+
+            case 5:
+                cout << "Saliendo de TrueDrive..." << endl;
+                break;
+
+            default:
+                cout << "No ha seleccionado una opción válida (1-5)." << endl;
+                break;
+        }
+
+        cout << "Desea continuar? (Y/N): ";
+        cin >> respuesta;
+    } while (respuesta == 'Y' || respuesta == 'y');
+
+    return 0;
 }
+
+bool checkForItemInt(const vector<int>& arr, int data) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr.at(i) == data) {
+            cout << "Data is already registered at position [" << i << "].\nPlease enter data again." << endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool checkForItemString(const vector<string>& arr, const string& data) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr.at(i) == data) {
+            cout << "Data is already registered at position [" << i << "].\nPlease enter data again." << endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+void printTable(const vector<int>& placa, const vector<string>& motor, const vector<string>& modelo) {
+    InfoClass t('-', '|', '+');
+    t.add("Order");
+    t.add("Placa");
+    t.add("Motor");
+    t.add("Modelo");
+    t.endOfRow();
+
+    for (int i = 0; i < placa.size(); i++) {
+        t.add(to_string(i));
+        t.add(to_string(placa.at(i)));
+        t.add(motor.at(i));
+        t.add(modelo.at(i));
+        t.endOfRow();
+    }
+
+    t.setAlignment(2, InfoClass::Alignment::RIGHT);
+    cout << t;
+}
+
