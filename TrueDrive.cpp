a677410e73cd31371ae8d4e684a05a1b3f291ffa
ojuniormunguia <oscarmunguiasotelo@gmail.com>
2023-06-10 15:31:22 -0600

    Last version

--- TrueDrive.cpp
+++ TrueDrive.cpp
@@ -5,6 +5,12 @@
 #include<conio.h>
 #include<queue>
 #include "InfoClass.h"
+#include <ctime>
+#include <chrono>
+#include <string>
+#include <cstdlib> 
+
+
 
 //Prueba
 
@@ -32,9 +38,29 @@ void printTableViajeTradicional
 	(const vector<int>& placa, const vector<string>& motor, const vector<string>& modelo, const vector<int>& year, 
 	const vector<string>& vnombre, const vector<string>& vapellido, const vector<int>& vdui, const vector<int>& vlicencia, 
 	const vector<int>& vnumisss, const vector<int>& vtelefono);
+void printTableHistorial
+	(const vector<int>& placa, const vector<string>& motor, const vector<string>& modelo, const vector<int>& year, 
+	const vector<string>& vnombre, const vector<string>& vapellido, const vector<int>& vdui, const vector<int>& vlicencia, 
+	const vector<int>& vnumisss, const vector<int>& vtelefono, const vector<string>& vhora);
+void printTableHistorialFiltered(const vector<int>& placa, const vector<string>& motor, const vector<string>& modelo, const vector<int>& year,
+    const vector<string>& vnombre, const vector<string>& vapellido, const vector<int>& vdui, const vector<int>& vlicencia,
+    const vector<int>& vnumisss, const vector<int>& vtelefono, const vector<string>& vhora, const string& searchQuery);
+void addCurrentTime(std::vector<std::string>& vhora) {
+    std::time_t currentTime = std::time(nullptr);  // Get the current time
 
+    std::tm* timeInfo = std::localtime(&currentTime);  // Convert to local time
+
+    char buffer[9];  // Buffer to store the formatted time
+    std::strftime(buffer, sizeof(buffer), "%H:%M:%S", timeInfo);  // Format the time
+
+    std::string timeString(buffer);  // Convert the buffer to a string
+
+    vhora.push_back(timeString);  // Add the time string to the vector
+}
 
 int main(){
+	
+	
     char respuesta = '\0';
     int control;
     int opciones;
@@ -46,6 +72,7 @@ int main(){
     int moverint;
 	string moverstring;
 	int seleccion;
+	string searchQuery;
 	    
     //U S E R   I N P U T S
     //Carros
@@ -61,6 +88,14 @@ int main(){
     int licencia;
     int numisss;
     int telefono;
+    
+    //Tiempo (identifica historial)
+    std::time_t currentTime;
+    std::string timeString;
+    char buffer[9];
+    std::tm* timeInfo = std::localtime(&currentTime);
+    std::strftime(buffer, sizeof(buffer), "%H:%M:%S", timeInfo);
+
     
     
     //V E C T O R E S   G E N E R A L E S
@@ -133,19 +168,49 @@ int main(){
     vector<int> vnumisssenviajeEjecutivo;
     vector<int> vtelefonoenviajeEjecutivo;
     
+	//V E C T O R E S   E N   H I S T O R I A L E S  (Ejecutivo)
+    //Carro
+    vector<int> placaHistorial;
+    vector<int> yearHistorial;
+    vector<string> motorHistorial;
+    vector<string> modeloHistorial;
+    //Conductor
+    vector<string> vnombreHistorial;
+    vector<string> vapellidoHistorial;
+    vector<int> vduiHistorial;
+    vector<int> vlicenciaHistorial;
+    vector<int> vnumisssHistorial;
+    vector<int> vtelefonoHistorial;
+    vector<string> vhora;
 
     cout << "Bienvenido a TrueDrive" << endl;
 
     do {
         system("cls");
-        cout << "Por favor seleccione una opcion:\n\n";
+        
+        // Get the current system time
+  						std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+
+  							// Convert the current system time to a time_t object
+  						std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+
+  						// Convert the time_t object to a string representation
+  						std::string currentTimeString = std::ctime(&currentTime);
+
+ 						 // Print the current time
+  						std::cout << "Current time: " << currentTimeString;
+						
+        cout << "Por favor seleccione una opci"<<char(162)<<"n:\n\n";
         cout << "\t\t1. Ingresar nuevo vehiculo y conductor" << endl;
         cout << "\t\t2. Ver lista de conductores" << endl;
         cout << "\t\t3. Comenzar Viaje" << endl;
         cout << "\t\t4. Terminar Viaje" << endl;
-        cout << "\t\t5. Salir del programa" << endl << endl;
+        cout << "\t\t5. Ver historial de viajes" << endl;
+        cout << "\t\t6. Salir del programa" << endl << endl;
         cout << "Escribir su opcion: ";
         cin >> opciones;
+        
+        
 
         switch (opciones) {
             case 1: //AGREGAR CONDUCTOR
@@ -155,7 +220,7 @@ int main(){
         	
                 do{
                 cout << " " << endl; 
-				cout << "Anio: ";
+				cout << "A"<<char(164)<<"o: ";
                 cin >> yeartaxi;
                 	
                     if(yeartaxi>=2015) {
@@ -240,7 +305,7 @@ int main(){
                 	//DUI ES INT (ÚNICO)
                 
                 	do{
-						cout<<"Ingrese el # de DUI sin guion: ";
+						cout<<"Ingrese el # de DUI sin gui"<<char(162)<<"n: ";
                 		cin>>dui;
 					}while(checkForItemInt(vdui,dui));
 					vdui.push_back(dui);
@@ -249,7 +314,7 @@ int main(){
 					//LICENCIA (ÚNICO)
 				
 					do{
-						cout<<"Ingresa el # de licencia sin guion: ";
+						cout<<"Ingresa el # de licencia sin gui"<<char(162)<<"n: ";
 						cin>>licencia;
 					}while(checkForItemInt(vlicencia,licencia));
 					vlicencia.push_back(licencia);
@@ -330,7 +395,7 @@ int main(){
                 	//DUI ES INT (ÚNICO)
                 
                 	do{
-						cout<<"Ingrese el # de DUI sin guion: ";
+						cout<<"Ingrese el # de DUI sin gui"<<char(162)<<"n: ";
                 		cin>>dui;
 					}while(checkForItemInt(vdui,dui));
 					vdui.push_back(dui);
@@ -339,7 +404,7 @@ int main(){
 					//LICENCIA (ÚNICO)
 				
 					do{
-						cout<<"Ingresa el # de licencia sin guion: ";
+						cout<<"Ingresa el # de licencia sin gui"<<char(162)<<"n: ";
 						cin>>licencia;
 					}while(checkForItemInt(vlicencia,licencia));
 					vlicencia.push_back(licencia);
@@ -357,7 +422,7 @@ int main(){
 					//NÚMERO DE TELÉFONO ES INT (ÚNICO)
 				
 					do{
-						cout<< "Ingresa el numero de telefono: ";
+						cout<< "Ingresa el numero de tel"<<char(130)<<"fono: ";
 						cin>>telefono;
 					}while(checkForItemInt(vtelefono,telefono));
 					vtelefono.push_back(telefono);
@@ -372,7 +437,7 @@ int main(){
             case 2:
                 // Ver lista de vehículos y conductores
                 cout << " " << endl; 
-				cout << "¿Cual lista desea ver?:\n\n";
+				cout << char(168)<<"cu"<<char(160)<<"l lista desea ver?:\n\n";
         		cout << "\t\t1. Lista de conductores totales" << endl;
         		cout << "\t\t2. Lista de conductores ejecutivos disponibles" << endl;
         		cout << "\t\t3. Lista de conductores Tradicionales disponibles" << endl;
@@ -412,7 +477,7 @@ int main(){
             case 3:
                 // COMIENZA VIAJE
                 cout << " " << endl; 
-				cout << "¿Que tipo de viaje desea iniciar?:\n\n";
+				cout << char(168)<<"Qu"<<char(130)<<" tipo de viaje desea iniciar?:\n\n";
         		cout << "\t\t1. Ejecutivo" << endl;
         		cout << "\t\t2. Tradicional" << endl;
                 
@@ -521,7 +586,7 @@ int main(){
             case 4:
                 // TERMINA VIAJE
                 cout << " " << endl; 
-				cout << "¿Que tipo de viaje desea Finalizar?:\n\n";
+				cout << char(168)<<"Qu"<<char(130)<<" tipo de viaje desea Finalizar?:\n\n";
         		cout << "\t\t1. Ejecutivo" << endl;
         		cout << "\t\t2. Tradicional" << endl;
                 
@@ -545,46 +610,72 @@ int main(){
         				moverint = placaenviajeEjecutivo[0+seleccion-1]; //usa "moverint/moverstring" para copiar primer elemento
         				placaenviajeEjecutivo.erase(placaenviajeEjecutivo.begin()); //elimina el primer elemento
 						placaEjecutivo.push_back(moverint); //pega el primer elemento en vector "enviaje"
+						placaHistorial.push_back(moverint);
 						
-						moverint = yearenviajeEjecutivo[0+seleccion-1];
 						yearenviajeEjecutivo.erase(yearenviajeEjecutivo.begin());
 						yearEjecutivo.push_back(moverint);    
+						yearHistorial.push_back(moverint);
 						
 						moverstring = motorenviajeEjecutivo[0+seleccion-1];
         				motorenviajeEjecutivo.erase(motorenviajeEjecutivo.begin());
 						motorEjecutivo.push_back(moverstring);
+						motorHistorial.push_back(moverstring);
 						
 						moverstring = modeloenviajeEjecutivo[0+seleccion-1];
 						modeloenviajeEjecutivo.erase(modeloenviajeEjecutivo.begin());
 						modeloEjecutivo.push_back(moverstring);    
+						modeloHistorial.push_back(moverstring); 
 						
 						moverstring = vnombreenviajeEjecutivo[0+seleccion-1];
         				vnombreenviajeEjecutivo.erase(vnombreenviajeEjecutivo.begin());
 						vnombreEjecutivo.push_back(moverstring);
+						vnombreHistorial.push_back(moverstring);
 						
 						moverstring = vapellidoenviajeEjecutivo[0+seleccion-1];
 						vapellidoenviajeEjecutivo.erase(vapellidoenviajeEjecutivo.begin());
 						vapellidoEjecutivo.push_back(moverstring);   
+						vapellidoHistorial.push_back(moverstring);  
 						
 						moverint = vduienviajeEjecutivo[0+seleccion-1];
         				vduienviajeEjecutivo.erase(vduienviajeEjecutivo.begin());
 						vduiEjecutivo.push_back(moverint);
+						vduiHistorial.push_back(moverint);
 						
 						moverint = vlicenciaenviajeEjecutivo[0+seleccion-1];
 						vlicenciaenviajeEjecutivo.erase(vlicenciaenviajeEjecutivo.begin());
-						vlicenciaEjecutivo.push_back(moverint);    
+						vlicenciaEjecutivo.push_back(moverint); 
+						vlicenciaHistorial.push_back(moverint);   
 						
 						moverint = vnumisssenviajeEjecutivo[0+seleccion-1];
         				vnumisssenviajeEjecutivo.erase(vnumisssenviajeEjecutivo.begin());
 						vnumisssEjecutivo.push_back(moverint);
+						vnumisssHistorial.push_back(moverint);
 						
 						moverint = vtelefonoenviajeEjecutivo[0+seleccion-1];
 						vtelefonoenviajeEjecutivo.erase(vtelefonoenviajeEjecutivo.begin());
 						vtelefonoEjecutivo.push_back(moverint); 
+						vtelefonoHistorial.push_back(moverint);
 						
-						cout << " " << endl; 
-						cout << "viaje ejecutivo Finalizado" << endl;
-        				cout << " " << endl; 
+						
+						// Get the current time
+            			currentTime = std::time(nullptr);
+
+            			// Convert the current time to a string without spaces
+            			
+            			timeInfo = std::localtime(&currentTime);
+            			char buffer[9];  // Buffer to store the formatted time
+
+            			// Format the time as HH:MM:SS
+            			std::strftime(buffer, sizeof(buffer), "%H:%M:%S", timeInfo);
+            			timeString = buffer;
+
+            			// Display the current time without spaces
+            			std::cout << "Viaje ejecutivo finalizado el: " << timeString << std::endl;
+            			
+            			addCurrentTime(vhora);						            			
+
+						
+						
 						break;
         			case 2:
         				//TRADICIONAL
@@ -645,19 +736,42 @@ int main(){
         				break;
         		}
                 break;
+			case 5:
+                cout << "Por favor seleccione una opci"<<char(162)<<"n:\n\n";
+        		cout << "\t\t1. Historial de todos los viajes" << endl;
+        		cout << "\t\t2. Historial de un conductor" << endl;
+        		cin>>opcionesviaje;
+        		switch(opcionesviaje) {
+        			case 1:
+        				printTableHistorial(placaHistorial, motorHistorial, modeloHistorial, yearHistorial, vnombreHistorial, vapellidoHistorial, vduiHistorial, vlicenciaHistorial, vnumisssHistorial, vtelefonoHistorial, vhora);//Imprime tabla completa
+        				
+        				break;
+        			case 2:
+        				cout << "Introduzca el nombre a buscar:\n\n";
+        				cin>>searchQuery;
+        				printTableHistorialFiltered(placaHistorial, motorHistorial, modeloHistorial, yearHistorial, vnombreHistorial, vapellidoHistorial, vduiHistorial, vlicenciaHistorial, vnumisssHistorial, vtelefonoHistorial, vhora, searchQuery);//Imprime tabla completa
+        				
+						break;
+					default:
+					cout << "No ha seleccionado una opci"<<char(162)<<"n v"<<char(160)<<"lida (1-5)." << endl;
+                	break;
+						
+				}
+                break;
 
-            case 5:
+                
+            case 6:
                 cout << "Saliendo de TrueDrive..." << endl;
                 system("pause");
                 system("exit");
                 break;
 
             default:
-                cout << "No ha seleccionado una opcion válida (1-5)." << endl;
+                cout << "No ha seleccionado una opci"<<char(162)<<"n v"<<char(160)<<"lida (1-5)." << endl;
                 break;
         }
 
-        cout << "¿Desea continuar? (Y/N): ";
+        cout << char(168)<<"Desea continuar? (Y/N): ";
         respuesta = getch();
     } while (respuesta == 'Y' || respuesta == 'y');
 }
@@ -665,7 +779,7 @@ int main(){
 bool checkForItemInt(const vector<int>& arr, int data) {
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] == data) {
-            cout << "Data is already registered at position [" << i << "].\nPlease enter data again.\n";
+            cout << "Estos datos est"<<char(160)<<"n repetidos en la posici"<<char(162)<<"n [" << i+1 << "].\nIntente otra vez.\n";
             return true;
         }
     }
@@ -675,7 +789,7 @@ bool checkForItemInt(const vector<int>& arr, int data) {
 bool checkForItemString(const vector<string>& arr, const string& data) {
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] == data) {
-            cout << "Data is already registered at position [" << i << "].\nPlease enter data again.\n";
+            cout << "Estos datos est"<<char(160)<<"n repetidos en la posici"<<char(162)<<"n [" << i+1 << "].\nIntente otra vez.\n";
             return true;
         }
     }
@@ -690,7 +804,7 @@ void printTable(const vector<int>& placa, const vector<string>& motor, const vec
     t.add("Placa");
     t.add("Motor");
     t.add("Modelo");
-    t.add("year");
+    t.add("Antiguedad");
     t.add("nombre");
     t.add("apellido");
     t.add("DUI");
@@ -725,7 +839,7 @@ void printTableEjecutiva(const vector<int>& placa, const vector<string>& motor, 
     t.add("Placa");
     t.add("Motor");
     t.add("Modelo");
-    t.add("year");
+    t.add("Antiguedad");
     t.add("nombre");
     t.add("apellido");
     t.add("DUI");
@@ -760,7 +874,7 @@ void printTableTradicional(const vector<int>& placa, const vector<string>& motor
     t.add("Placa");
     t.add("Motor");
     t.add("Modelo");
-    t.add("year");
+    t.add("Antiguedad");
     t.add("nombre");
     t.add("apellido");
     t.add("DUI");
@@ -797,7 +911,7 @@ void printTableViajeEjecutiva(const vector<int>& placa, const vector<string>& mo
     t.add("Placa");
     t.add("Motor");
     t.add("Modelo");
-    t.add("year");
+    t.add("Antiguedad");
     t.add("nombre");
     t.add("apellido");
     t.add("DUI");
@@ -833,7 +947,7 @@ void printTableViajeTradicional(const vector<int>& placa, const vector<string>& 
     t.add("Placa");
     t.add("Motor");
     t.add("Modelo");
-    t.add("year");
+    t.add("Antiguedad");
     t.add("nombre");
     t.add("apellido");
     t.add("DUI");
@@ -855,6 +969,97 @@ void printTableViajeTradicional(const vector<int>& placa, const vector<string>& 
         t.add(to_string(vnumisss[i])); //agrega seguro
         t.add(to_string(vtelefono[i])); //agrega telefono
         t.endOfRow();
+    }
+
+    t.setAlignment(2, InfoClass::Alignment::RIGHT);
+    cout << t;
+}
+
+void printTableHistorial(const vector<int>& placa, const vector<string>& motor, const vector<string>& modelo, const vector<int>& year, 
+    const vector<string>& vnombre, const vector<string>& vapellido, const vector<int>& vdui, const vector<int>& vlicencia, 
+    const vector<int>& vnumisss, const vector<int>& vtelefono, const vector<string>& vhora) { 
+
+    InfoClass t('-', '|', '+');
+    t.add("Orden");
+    t.add("Placa");
+    t.add("Motor");
+    t.add("Modelo");
+    t.add("Antiguedad");
+    t.add("nombre");
+    t.add("apellido");
+    t.add("DUI");
+    t.add("licencia");
+    t.add("seguro");
+    t.add("telefono");
+    t.add("hora");
+    t.endOfRow();
+
+    for (int i = 0; i < placa.size(); i++) {
+        t.add(to_string(i+1)); //agrega el orden
+        t.add(to_string(placa[i])); //agrega el PLACA
+        t.add(motor[i]); //agrega el MOTOR
+        t.add(modelo[i]); //agrega el MODELO
+        t.add(to_string(year[i])); //agrega el AÑO
+        t.add(vnombre[i]); //agrega nombre
+        t.add(vapellido[i]); //agrega appellido
+        t.add(to_string(vdui[i])); //agrega dui
+        t.add(to_string(vlicencia[i])); //agrega licencia
+        t.add(to_string(vnumisss[i])); //agrega seguro
+        t.add(to_string(vtelefono[i])); //agrega telefono
+        t.add(vhora[i]); // Convert char to string
+        t.endOfRow();
+    }
+
+    t.setAlignment(2, InfoClass::Alignment::RIGHT);
+    cout << t;
+}
+
+void printTableHistorialFiltered(const vector<int>& placa, const vector<string>& motor, const vector<string>& modelo, const vector<int>& year,
+    const vector<string>& vnombre, const vector<string>& vapellido, const vector<int>& vdui, const vector<int>& vlicencia,
+    const vector<int>& vnumisss, const vector<int>& vtelefono, const vector<string>& vhora, const string& searchQuery) {
+
+    InfoClass t('-', '|', '+');
+    t.add("Orden");
+    t.add("Placa");
+    t.add("Motor");
+    t.add("Modelo");
+    t.add("Antiguedad");
+    t.add("nombre");
+    t.add("apellido");
+    t.add("DUI");
+    t.add("licencia");
+    t.add("seguro");
+    t.add("telefono");
+    t.add("hora");
+    t.endOfRow();
+
+    for (int i = 0; i < placa.size(); i++) {
+        if (searchQuery.empty() ||
+            to_string(placa[i]).find(searchQuery) != string::npos ||
+            motor[i].find(searchQuery) != string::npos ||
+            modelo[i].find(searchQuery) != string::npos ||
+            to_string(year[i]).find(searchQuery) != string::npos ||
+            vnombre[i].find(searchQuery) != string::npos ||
+            vapellido[i].find(searchQuery) != string::npos ||
+            to_string(vdui[i]).find(searchQuery) != string::npos ||
+            to_string(vlicencia[i]).find(searchQuery) != string::npos ||
+            to_string(vnumisss[i]).find(searchQuery) != string::npos ||
+            to_string(vtelefono[i]).find(searchQuery) != string::npos ||
+            vhora[i].find(searchQuery) != string::npos) {
+            t.add(to_string(i + 1)); //agrega el orden
+            t.add(to_string(placa[i])); //agrega el PLACA
+            t.add(motor[i]); //agrega el MOTOR
+            t.add(modelo[i]); //agrega el MODELO
+            t.add(to_string(year[i])); //agrega el AÑO
+            t.add(vnombre[i]); //agrega nombre
+            t.add(vapellido[i]); //agrega appellido
+            t.add(to_string(vdui[i])); //agrega dui
+            t.add(to_string(vlicencia[i])); //agrega licencia
+            t.add(to_string(vnumisss[i])); //agrega seguro
+            t.add(to_string(vtelefono[i])); //agrega telefono
+            t.add(vhora[i]); // Convert char to string
+            t.endOfRow();
+        }
     }
 
     t.setAlignment(2, InfoClass::Alignment::RIGHT);
